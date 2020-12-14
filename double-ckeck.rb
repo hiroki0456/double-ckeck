179c8f539addb019b7e1a69969eeabdca264c0c0
hiroki0456 <hiroki.mugikura@di-v.co.jp>
2020-12-14 22:51:20 +0900

    modified sort

--- double-ckeck.rb
+++ double-ckeck.rb
@@ -1,7 +1,6 @@
-# APIのtoken
 require 'slack'
 require 'Date'
-require 'TIme'
+require 'Time'
 require 'logger'
 # slack api token
 token = ENV['SLACK_API_TOKEN']
@@ -14,24 +13,29 @@ oldest = Time.parse(yesterday + " 22:00:00").to_i
 # 取得したい日付（to)
 latest = Time.parse(today + " 10:00:00").to_i
 
-# messageの取得
+# 受講生名とタイムスタンプの取得
 def get_student_and_ts(client, oldest, latest)
   # メッセージの取得
   messages = client.conversations_history(channel: 'C014KGE02SK', oldest: oldest, latest: latest, count: 1000)['messages']
+  # 日付の並び替え
+  new_messages = messages.sort{|a, b| a['ts'] <=> b['ts']}
   # 名前をいれる配列
   student_arr = []
   # 日付をいれる配列
   ts_arr = []
+  # 重複受講生の確認
+  double_student_arr = []
   # 受講生とタイムスタンプを取得
-  messages.each do |msg|
+  new_messages.each do |msg|
     begin
       # 受講生名の取得
       student_name = msg['attachments'][0]['fields'][0]['value']
       # タイムスタンプの取得
       ts = msg['ts']
-      # 受講生が被っていたら日付を配列にいれる
+      # 受講生が被っていたら日付と受講生名を配列にいれる
       if student_arr.include?(student_name)
         ts_arr << ts
+        double_student_arr << student_name
         # 被ってなかったら受講生を配列にいれる
       else
         student_arr << student_name
@@ -41,18 +45,18 @@ def get_student_and_ts(client, oldest, latest)
       logger.error(exception)
     end
   end
-  stamp_add(ts_arr)
+  # メソッドの呼び出し
+  stamp_add(client, ts_arr)
 end
 
   # 日付のメッセージにスタンプを押す
-def stamp_add(ts_arr)
-  p ts_arr
-  # コメントアウトを外す
-  # ts_arr.each do |ts|
-  #   client.reactions_add(channel: 'C014KGE02SK', name: "chouhuku", timestamp: ts)
-  # end
+def stamp_add(client, ts_arr)
+  ts_arr.each do |ts|
+    client.reactions_add(channel: 'C014KGE02SK', name: "chouhuku", timestamp: ts)
+  end
 end
 
 # インスタンスの生成
 client = Slack::Client.new(token: token)
+# メソッドの呼び出し
 get_student_and_ts(client, oldest, latest)
